6c72c7b590251e8699646154daf7b712a9682e2f
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-29 11:32:13 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vahtee Boo on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
